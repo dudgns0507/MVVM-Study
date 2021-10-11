c69453e3898271eba176975a8f5d1f2cc38386aa
copin-danny <hoon2585@gmail.com>
2021-10-11 09:41:30 +0900

    commit

--- app/src/main/java/com/github/dudgns0507/mvvm_cropo/App.kt
+++ app/src/main/java/com/github/dudgns0507/mvvm_cropo/App.kt
@@ -1,7 +1,7 @@
 package com.github.dudgns0507.mvvm_cropo
 
 import android.app.Application
-import com.github.dudgns0507.core.pref.PrefManager
+import com.github.dudgns0507.kref.KrefManager
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -9,6 +9,6 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        PrefManager.initialize(this)
+        KrefManager.init(this)
     }
 }
